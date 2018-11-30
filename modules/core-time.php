13fd13ee49e4c5930a7f66d766d305c19300cdfb
Vivek Nair <vnair746@stanford.edu>
2018-11-30 02:19:19 -0600

    Added basic module design
    Added HTML outline for time module

---
+++ modules/core-time.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Time | Core Modules | Dynamics</title>
+		<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+		<link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500|Roboto:300,400,500,700" rel="stylesheet">
+		<link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-red.min.css">
+	</head>
+	<body>
+		<style>
+			.timezone {
+				text-align: center;
+			}
+			.timezone h1.current-time {
+				display: block;
+				font-weight: 300;
+				font-family: "Roboto", sans-serif;
+				color: rgba(0,0,0,0.87);
+				margin: 0;
+			}
+			.timezone p.current-time-zone {
+				display: block;
+				line-height: 32px;
+				font-weight: 300;
+				font-family: "Roboto", sans-serif;
+				margin: 0;
+				font-size: 22px;
+			}
+			.mdl-card__supporting-text {
+				margin: auto;
+			}
+		</style>
+		<div class="mdl-card__supporting-text">
+			<div class="timezone">
+				<h1 class="current-time">02:14:37 AM</h1>
+				<p class="current-time-zone">America/Chicago (UTC-6)</p>
+			</div>
+		</div>
+
+	</body>
+</html>
