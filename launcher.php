cd9789088743f3caf76b551fdb27b60257057278
Vivek Nair <vnair746@stanford.edu>
2018-12-09 20:27:46 -0600

    Update launcher.php

--- launcher.php
+++ launcher.php
@@ -25,7 +25,8 @@
   <body>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script>
-      
+      var popup = window.open("index.html", "popup", "width=610,height=310,scrollbars=no,resizable=no");
+      window.resizeTo(610, 310);
     </script>
   </body>
 </html>
